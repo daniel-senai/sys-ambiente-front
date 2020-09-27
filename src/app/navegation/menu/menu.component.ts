4b8b28fbabb0b9f0628ea0d488cabfffbfd0bfd4
Anisberto Reis <anisbertoos@gmail.com>
2020-09-26 23:25:39 -0300

    creation of the menu component

--- src/app/navegation/menu/menu.component.ts
+++ src/app/navegation/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
   constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+     
   }
 }
