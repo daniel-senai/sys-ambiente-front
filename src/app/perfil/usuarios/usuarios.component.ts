4a1ff839ad0f2600053c2bea41cc7d6fbc9cafb8
Anisberto Reis <anisbertoos@gmail.com>
2020-10-10 23:38:04 -0300

    Tabela Usuarios e ajuste de Rotas

--- src/app/perfil/usuarios/usuarios.component.ts
+++ src/app/perfil/usuarios/usuarios.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from './../service/usuario.service';
+import { ErrorMsgComponent } from './../../share/error-msg/error-msg.component';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from './../interfaces/usuario';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-usuarios',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
+  public usuarios: Usuario[];
+  usuarioSelecionado: Usuario;
 
-  constructor() { }
+  @ViewChild(ErrorMsgComponent) errorMsgComponent: ErrorMsgComponent;
+  constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
+    this.usuarioService.getListaUsuarios().subscribe((user: Usuario[]) => { this.usuarios = user })
+  }
+  deletarUsuario(id: number) {
+    this.usuarioService.deleteUsuario(id)
+      .subscribe(() => {
+        this.usuarioService.getListaUsuarios();
+      }, () => { this.errorMsgComponent.setError('Falha ao Deletar Usuario'); });
   }
 
 }
