ae449e05b9873fa3c739fcd1c48e4f3d2816f921
Anisberto Reis <anisbertoos@gmail.com>
2020-10-20 21:22:27 -0300

    Config menu telas

--- src/app/navegacao/ambiente/ambiente.component.ts
+++ src/app/navegacao/ambiente/ambiente.component.ts
@@ -1,3 +1,4 @@
+import { Ambiente, Regional } from './../interfaces/ambiente';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ambiente.component.css']
 })
 export class AmbienteComponent implements OnInit {
+  regional: Regional[];
+  regionalSelecionada: Regional;
 
-  constructor() { }
+  constructor() {
+
+    
+    this.regional = [];
+        for (let i = 0; i < 10000; i++) {
+            this.regional.push({id:  i, regional: 'Regional' + i});
+        }
+
+    this.regional = [
+      { regional: 'DRGO', id: 1 },
+      { regional: 'DRSP', id: 2 },
+    ]
+  }
 
   ngOnInit(): void {
   }
-
 }
